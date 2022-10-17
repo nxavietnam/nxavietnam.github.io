962cb6ae51e06043080e3f8d2b235fab44a3f4c0
nxavietnam <49309774+nxavietnam@users.noreply.github.com>
2022-10-17 20:41:05 +1100

    xoa

--- vega_lite_bubble.js
+++
@@ -1,5 +0,0 @@
-var vg_5 = "bubble_plot_final.vg.json";
-vegaEmbed("#bubble_chart", vg_5).then(function(result) {
-
-
-}).catch(console.error);
--- vega_lite_line.js
+++
@@ -1,4 +0,0 @@
-var vg_4 = "line_chart.vg.json";
-vegaEmbed("#line_chart", vg_4).then(function(result) {
-
-}).catch(console.error);
