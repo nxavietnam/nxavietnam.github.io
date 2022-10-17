94d100695aa1e042dfb06041bf8d14cce5a397ab
nxavietnam <49309774+nxavietnam@users.noreply.github.com>
2022-10-17 20:30:40 +1100

    final

---
+++ vega_lite_bar.js
@@ -0,0 +1,5 @@
+var vg_3 = "bar_chart_consumption.vg.json";
+vegaEmbed("#bar_chart", vg_3).then(function(result) {
+
+
+}).catch(console.error);
---
+++ vega_lite_map.js
@@ -0,0 +1,5 @@
+var vg_1 = "map_oil_usage.vg.json";
+vegaEmbed("#map_chart", vg_1).then(function(result) {
+
+
+}).catch(console.error);
